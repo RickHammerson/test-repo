f517d49286cd49014cf932b544375e2dafa12cd3
Rick <RickHammerson@ya.ru>
2024-04-03 22:40:50 +0300

    Добавление NewFile.txt

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Открытие магазина ожидается через 2 месяца или 4 дня");
+        System.out.println("Строка для коммента в гитхабе");
     }
 }
